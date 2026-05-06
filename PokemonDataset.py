930058ab70a7159ea8efea08cf08d5d5fd7a307b
chooboy <chooboy05@gmail.com>
2026-05-05 21:30:00 -0400

    added horizontal flipping to help training

--- PokemonDataset.py
+++ PokemonDataset.py
@@ -15,6 +15,7 @@ class PokemonDataset(Dataset):
         self.images = sorted([f for f in os.listdir(img_dir) if f.endswith('.png')])
         self.transform = transforms.Compose([
             transforms.Resize((image_size, image_size)),
+            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(), # [0, 255] → [0, 1]
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]) # [0, 1] → [-1, 1]
         ])
